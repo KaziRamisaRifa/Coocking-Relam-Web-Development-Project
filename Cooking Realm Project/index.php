e6e39e7acf17b0f8dde7e9f447f48603880d7079
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-01-13 21:48:44 +0600

    Create index.php

---
+++ Cooking Realm Project/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Cooking Realm Website</title>
+  <!-- For all types view-->
+  <meta name="viewport" content="width=device-wiidth,initial-scale=1">
+
+  <!-- style.css-->
+  <link rel="stylesheet" href="CSS/style.css">
+
+  <!-- Bootstrap Grid System-->
+  <link rel="stylesheet" href="CSS/bootstrap-grid.css">
+
+
+  </head>
+
+  <body>
+
+    <header>
+      <?php include 'header.php';?>
+                </header>
+
+<!-- Background Section-->
+      <div class="bground">
+        <div class="bground__text">
+          <h1>Welcome to Cooking Realm</h1>
+          <p>Food That You Can't Resist</p>
+          <a class="botton margin__top" href="front.php">Get Started</a>
+        </div>
+      </div>
+
+  </body>
+</html>
