083b33fe41f2f5d2a967b5546e5850969d93f84f
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-01-13 21:48:13 +0600

    update

---
+++ Cooking Realm Project/footer.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <!-- style.css-->
+    <link rel="stylesheet" href="CSS/style.css">
+
+    <!-- Bootstrap Grid System-->
+    <link rel="stylesheet" href="CSS/bootstrap-grid.css">
+
+  </head>
+  <body>
+    <!-- FeedBack section-->
+    <div class="FeedBack">
+
+    </div>
+    <!-- Developed section-->
+    <div class="Developed__By">
+    <div class="container">
+      <div class="row">
+        <div class="section-title">
+          <h1>Designed And Developed By</h1>
+          <p>We strategically design and develop websites that elevate your brand and communicate your message effectively. </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="team-members">
+
+          <div class="member">
+            <img src="Images/member1.jpg">
+
+            <div class="inner">
+              <div class="info">
+                <h5>Farida Yeasmin Mim</h5>
+                <p>Designer</p>
+                <div class="social-links">
+                  <a href=""><span class="fa fa-facebook"></span></a>
+                  <a href=""><span class="fa fa-instagram"></span></a>
+                  <a href=""><span class="fa fa-twitter"></span></a>
+                  <a href=""><span class="fa fa-linkedin"></span></a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="member">
+            <img src="Images/member2.jpg">
+
+            <div class="inner">
+              <div class="info">
+                <h5>Kazi Ramisa Rifa</h5>
+                <p>Designer</p>
+                <div class="social-links">
+                  <a href=""><span class="fa fa-facebook"></span></a>
+                  <a href=""><span class="fa fa-instagram"></span></a>
+                  <a href=""><span class="fa fa-twitter"></span></a>
+                  <a href=""><span class="fa fa-linkedin"></span></a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="member">
+            <img src="Images/Abid.jpg">
+
+            <div class="inner">
+              <div class="info">
+                <h5>Abid Hossain Fahim</h5>
+                <p>Designer</p>
+                <div class="social-links">
+                  <a href=""><span class="fa fa-facebook"></span></a>
+                  <a href=""><span class="fa fa-instagram"></span></a>
+                  <a href=""><span class="fa fa-twitter"></span></a>
+                  <a href=""><span class="fa fa-linkedin"></span></a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    </div>
+
+
+  </body>
+</html>
---
+++ Cooking Realm Project/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <!-- style.css-->
+    <link rel="stylesheet" href="CSS/style.css">
+
+    <!-- Bootstrap Grid System-->
+    <link rel="stylesheet" href="CSS/bootstrap-grid.css">
+
+  </head>
+  <body>
+
+      <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+          <i class="fas fa-bars"></i>
+        </label>
+        <label class= "logo">CookingRealm</label>
+        <ul>
+          <li><a class="active" href="index.php">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Recipes</a></li>
+          <li><a href="#">Contest</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Log In</a></li>
+
+        </ul>
+      </nav>
+
+  </body>
+</html>
