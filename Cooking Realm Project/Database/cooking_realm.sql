d3c3612e7b3365863d147f13950b664bee1e0911
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-01-17 18:48:06 +0600

    Database

---
+++ Cooking Realm Project/Database/cooking_realm.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 17, 2021 at 01:36 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cooking_realm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `Name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Comments` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`Name`, `Email`, `Comments`) VALUES
+('Kazi Ramisa Rifa', 'kazi.rifa@northsouth.edu', 'I love this Site. '),
+('Rifa', 'ramisa.rifa09@gmail.com', 'Wonderful Site'),
+('Rifa', 'ramisa.rifa09@gmail.com', 'Wonderful Site'),
+('rifa', 'ramisa.rifa09@gmail.com', 'Cooooookinggggg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `User_ID` bigint(20) NOT NULL,
+  `User_Name` varchar(30) NOT NULL,
+  `First_Name` varchar(45) NOT NULL,
+  `Last_Name` varchar(45) NOT NULL,
+  `Date_of_birth` date NOT NULL,
+  `Country` varchar(20) NOT NULL,
+  `Contact_Number` varchar(30) DEFAULT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  `Premium_ID` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`User_ID`, `User_Name`, `First_Name`, `Last_Name`, `Date_of_birth`, `Country`, `Contact_Number`, `Email`, `Password`, `Premium_ID`) VALUES
+(1, 'rifa', 'Ramisa', 'Rifa', '2000-01-29', 'Bangladesh', '018796484', 'ramisa.rifa09@gmail.com', 'abcd1234', NULL),
+(2, 'mimdim', 'Mim', 'Dim', '2000-11-01', 'Japan', '0128689469', 'mimdim@cookingrealm.com', '1234abcd', NULL),
+(3, 'MimRami', 'Ramisa', 'Mim', '2000-02-02', 'Kuwait', '002020202', 'mramim@gmail.com', 'MimRamisa', NULL),
+(5, 'AnkiBaby', 'Ankita', 'Baby', '1955-04-05', 'Germany', '03939393993', 'anki.baby@gmail.com', 'ramisa', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`User_ID`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD UNIQUE KEY `Password` (`Password`),
+  ADD UNIQUE KEY `User_Name` (`User_Name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `User_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
