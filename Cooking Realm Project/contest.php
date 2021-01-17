2dbc35eb8b6672990270ff448db22b113fdc8b77
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-01-17 22:08:51 +0600

    Create contest.php

---
+++ Cooking Realm Project/contest.php
@@ -0,0 +1,19 @@
+<?php
+include 'connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <header>
+      <?php include 'header.php';?>
+    </header>
+    
+  </body>
+
+</html>
